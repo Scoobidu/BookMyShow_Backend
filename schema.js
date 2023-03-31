74f9ee9cafbef1b705ecf49f713932291e4f51bc
unknown <nagleeesahil@gmail.com>
2023-04-01 04:43:19 +0530

    seats schema updated

--- schema.js
+++ schema.js
@@ -3,14 +3,7 @@ const { Schema } = require("mongoose");
 const bookMovieSchema = new Schema({
   movie: Schema.Types.String,
   slot: Schema.Types.String,
-  seats: {
-    A1: Schema.Types.Number,
-    A2: Schema.Types.Number,
-    A3: Schema.Types.Number,
-    A4: Schema.Types.Number,
-    D1: Schema.Types.Number,
-    D2: Schema.Types.Number,
-  },
+  seats: Schema.Types.Object,
 });
 
 exports.bookMovieSchema = bookMovieSchema;
